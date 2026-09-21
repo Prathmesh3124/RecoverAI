75719c0f7aa1881ab315952e6752f23c786b7c30
Prathmesh3124 <prathmeshs072004@gmail.com>
2026-09-21 23:19:57 +0530

    Allow deployed frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://127.0.0.1:5173"
+                   "https://recoverai-frontend-p7xy.onrender.com",
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
